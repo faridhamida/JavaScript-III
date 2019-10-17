68208bfbb49b3740cbd7c248c99b9a3c861ceb1b
Farid Hamida <faridhamida22@gmail.com>
2019-10-16 20:47:08 -0700

    Completed challenge for JavaScript III

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. When a function is contained in global scope the value of this inside that function will be the window object.
+* 2. Everything to the left of the dot in a function is what this is refering too
+* 3. When you call an object it will immediatley invoke the function
+* 4. New binding you create a new keyword for a new object and points to it.
 *
 * write out a code example of each explanation above
 */
@@ -12,15 +12,46 @@
 // Principle 1
 
 // code example for Window Binding
+function sayAge () {
+    console.log(`My age is ${this.age}`)
+}
 
+const user = {
+    name: "Farid",
+    age:25
+}
+sayAge();
 // Principle 2
 
 // code example for Implicit Binding
+let myCat = {
+    name: "Meowth",
+    speech: "English",
+    cat: function () {
+        console.log(this.speech);
+    }
+}
+myCat.cat();
 
 // Principle 3
 
 // code example for New Binding
+function Dog(saying) {
+    this.phase = saying;
+}
 
+let myDog = new Dog("Woof Woof");
+
+console.log(myDog.phase);
 // Principle 4
 
 // code example for Explicit Binding
+function pokemon() {
+    console.log(this.type);
+}
+
+let myPokemon = {
+    name: "Charmander",
+    type: "Fire",
+}
+pokemon.call(myPokemon);
